5d3b04c239df435b84da0cd8368d47912004992a
Yevhenii <kz.greenlinefamily@gmail.com>
2020-11-06 21:35:45 +0100

    already contact form

--- js/file-change.js
+++ js/file-change.js
@@ -2,13 +2,21 @@
 
 (function () {
 
-  const formFile = document.querySelector(`#file`);
-  const fileLabel = document.querySelector(`.contact-form__file-label`);
+  const formFile = document.querySelector(`.contact-form__wrap #file`);
+  const contactFormFile = document.querySelector(`#contact-file`);
+  const fileLabel = document.querySelector(`.contact-form__wrap .contact-form__file-label`);
+  const contactFileLabel = document.querySelector(`.contact .contact-form__file-label`);
 
-  function fileChangeHandler() {
-    fileLabel.textContent = formFile.value;
+  function fileChangeHandler(input, label) {
+    label.textContent = input.value;
   }
 
-  formFile.addEventListener(`change`, fileChangeHandler);
+  formFile.addEventListener(`change`, function() {
+    fileChangeHandler(formFile, fileLabel);
+  });
+
+  contactFormFile.addEventListener(`change`, function() {
+    fileChangeHandler(contactFormFile, contactFileLabel);
+  });
 
 })();
