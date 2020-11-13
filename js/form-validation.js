353da37e3b460b55e9423dcecab836b2a289ab39
Yevhenii <kz.greenlinefamily@gmail.com>
2020-11-13 21:53:18 +0100

    form validation start

--- js/form-validation.js
+++ js/form-validation.js
@@ -3,7 +3,7 @@
 (function () {
   const modalForm = window.openForm.formWrap;
   const contactForm = document.querySelector('.contact-form');
-  const modalFormCheck = modalForm.querySelector('#policy');
+  /*const modalFormCheck = modalForm.querySelector('#policy');
   const contactFormCheck = contactForm.querySelector('#contact-policy');
   const modalFormSubmit = modalForm.querySelector('button');
   const contactFormSubmit = contactForm.querySelector('button');
@@ -22,5 +22,75 @@
 
   contactFormSubmit.addEventListener('click', function (evt) {
     isPolicyChecked(contactForm, contactFormCheck, evt);
-  })
+  })*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const username = document.getElementById('name');
+  const email = document.getElementById('email');
+  const phone = document.getElementById('phone');
+  const contactFormSubmit = contactForm.querySelector('button');
+
+
+  username.addEventListener('input', function () {
+    showSuccess(username);
+  });
+
+
+  email.addEventListener('input', function () {
+    checkEmail(email);
+  });
+
+
+  function showError(input) {
+    input.classList.add('invalid');
+    input.classList.remove('valid');
+  }
+
+  function showSuccess(input) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
+  }
+
+  function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())) {
+      showSuccess(input)
+    }else {
+      showError(input);
+    }
+  }
+
+//checkRequired fields
+  function checkRequired(inputArr) {
+    inputArr.forEach(function(input){
+      if(input.value.trim() === ''){
+        showError(input)
+      }else {
+        showSuccess(input);
+      }
+    });
+  }
+
+
+//Event Listeners
+  contactForm.addEventListener('submit',function(e) {
+    e.preventDefault();
+    checkRequired([username, email, phone]);
+    checkEmail(email);
+
+    if ([username, email, phone].classList.contains('valid')) {
+      contactForm.submit();
+    }
+  });
 })();
