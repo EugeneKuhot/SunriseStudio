1254e6c470f7955a765008dd9eadad994268c064
Yevhenii <kz.greenlinefamily@gmail.com>
2020-12-01 23:09:36 +0100

    burger

--- js/burger.js
+++ js/burger.js
@@ -3,11 +3,22 @@
 (function () {
   const burgerBtn = document.querySelector(`.burger-btn`);
   const burgerIcon = burgerBtn.querySelector(`svg`);
+  const burgerMenu = document.querySelector(`.header__burger-wrap`);
 
   function changeIcon() {
     burgerIcon.classList.toggle('active');
   }
 
-  burgerBtn.addEventListener(`click`, changeIcon)
+  function toggleMenu() {
+    burgerMenu.classList.toggle('slide-in-left');
+    burgerMenu.classList.toggle('slide-out-left');
+  }
+
+  function toggleBurger() {
+    changeIcon();
+    toggleMenu();
+  }
+
+  burgerBtn.addEventListener(`click`, toggleBurger)
 
 })();
